dec894a8f65078006fc4545f4d7b3aff0d8af78b
Leivi0116 <124672071+Leivi0116@users.noreply.github.com>
2025-11-02 18:19:26 +0800

    Update watch-party-hero.tsx

--- components/watch-party-hero.tsx
+++ components/watch-party-hero.tsx
@@ -51,7 +51,7 @@ export default function WatchPartyHero({ onContinue }: WatchPartyHeroProps) {
               <div className="pt-4">
 
 
-                <p>Second Batch Will announce Soon..</p>
+                <p>Thank you for your excitement, Summoners! <br />The official ticket selling will be announced soon on the official League of Legends Philippines social media pages</p>
 
 {/* 
                 
